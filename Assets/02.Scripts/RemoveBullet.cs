32e164bf49d4ea1df07a7286cb49e21783426dc9
Lee Jae Hyun <myleje@gmail.com>
2021-09-17 10:01:04 +0900

    .

--- Assets/02.Scripts/RemoveBullet.cs
+++ Assets/02.Scripts/RemoveBullet.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class RemoveBullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnCollisionEnter(Collision coll)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //if (coll.collider.tag == "BULLET")
+        if (coll.collider.CompareTag("BULLET"))
+        {
+            Destroy(coll.gameObject);
+        }
     }
 }
